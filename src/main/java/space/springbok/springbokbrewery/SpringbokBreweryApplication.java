6d5ae9dc25eaa67666110d8551c6b39165fe85a4
johnspangenberg <dev@springbok.space>
2024-03-09 14:41:07 +0100

    Initial commit

---
+++ src/main/java/space/springbok/springbokbrewery/SpringbokBreweryApplication.java
@@ -0,0 +1,13 @@
+package space.springbok.springbokbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbokBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbokBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/springbok/springbokbrewery/SpringbokBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package space.springbok.springbokbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbokBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
