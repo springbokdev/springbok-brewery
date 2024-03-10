fd7fabb518c9d4e36372c528f8603fd4fae0b4c2
johnspangenberg <dev@springbok.space>
2024-03-10 15:51:10 +0100

    Fixed BeerController tests.

--- src/test/java/space/springbok/springbokbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/space/springbok/springbokbrewery/web/controller/BeerControllerTest.java
@@ -53,7 +53,7 @@ public class BeerControllerTest {
 
         mockMvc.perform(get("/api/v1/beer/" + validBeer.getId().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is("Beer1")));
     }
@@ -68,7 +68,7 @@ public class BeerControllerTest {
 
         given(beerService.save(any())).willReturn(savedDto);
 
-        mockMvc.perform(post("/api/v1/beer/")
+        mockMvc.perform(post("/api/v1/beer")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
                 .andExpect(status().isCreated());
